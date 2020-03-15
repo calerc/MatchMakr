49353cec3e77ecc29a2e436c2fb6c8908db2495a
Cale Crowder <dcc42@zips.uakron.edu>
2020-03-15 16:50:30 -0400

    pyqt_app changed code to return handle for each widget

--- pyqt_app.py
+++ pyqt_app.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QDockWidget, QListWidget, QHBoxLayout, QVBoxLayout, QSpacerItem, QListWidgetItem
 from PyQt5.QtWidgets import QApplication, QTextEdit, QAction, QPushButton, QFrame, QGridLayout, QSizePolicy, QLabel
-from PyQt5.QtWidgets import QStackedWidget, QFileDialog
+from PyQt5.QtWidgets import QStackedWidget, QFileDialog, QSpinBox, QCheckBox, QLineEdit
 from itertools import product
 from ipdb import set_trace
 from os import getcwd
@@ -42,68 +42,128 @@ class SettingsFrame(QFrame):
     
     def __init__(self, q_main_window):
         super(SettingsFrame, self).__init__()
-        # self.settings_frame = QFrame()
-        # self.button_frame = QFrame()
-        # self.addWidget(self.settings_frame)
-        # self.addWidget(self.button_frame)
-        # self.setLayout(QGridLayout())
+        
+        # Split the frame
         self.split_frame = QVBoxLayout(self)
         self.settings_frame = QFrame()
         self.button_frame = QFrame()
         self.split_frame.addWidget(self.settings_frame)
         self.split_frame.addWidget(self.button_frame)
         self.settings_grid = QGridLayout(self.settings_frame)
+        self.button_layout = QHBoxLayout(self.button_frame)
         
+        # Store the parent
         self.q_main_window = q_main_window
-        self.define_labels()
-        self.define_text_boxes()
-        self.define_buttons()
-        self.populate_frame()
+        # self.define_labels()
+        # self.define_text_boxes()
+        # self.define_buttons()
         
-    def define_labels(self):
-        self.labels = {(0, 0): "Student Preferences",
-                       (1, 0): "Faculty Preferences",
-                       (2, 0): "Student Availability",
-                       (3, 0): "Faculty Availability",
-                       (4, 0): "Results Path",
-                       (5, 0): "Student Schedules Path",
-                       (6, 0): "Faculty Schedules Path",
-                       (7, 0): "Number of Interviews",
-                       (8, 0): "Minimum Number of Interviews",
-                       (9, 0): "Maximum Number of Interviews",
-                       (10, 0): "Reccomend Extra Matches",
-                       (11, 0): "Faculty Advantage Factor"}       
+        # Populate the settings
+        self.define_settings()
+        # self.populate_settings()
+        
+    def define_settings(self):
+        self.define_labels()
+        self.define_io()
+        self.define_controls()        
+    
+        
+    def define_labels(self):        
+        self.labels = {(0, 0): "Student Preferences:",
+                       (1, 0): "Faculty Preferences:",
+                       (2, 0): "Student Availability:",
+                       (3, 0): "Faculty Availability:",
+                       (4, 0): "Results Path:",
+                       (5, 0): "Student Schedules Path:",
+                       (6, 0): "Faculty Schedules Path:",
+                       (7, 0): "Number of Interviews:",
+                       (8, 0): "Minimum Number of Interviews:",
+                       (9, 0): "Maximum Number of Interviews:",
+                       (10, 0): "Reccomend Extra Matches:",
+                       (11, 0): "Faculty Advantage Factor:"}
+        
+        for pos, name in self.labels.items():
+                x, y = pos
+                ob = QLabel(self.settings_frame)
+                ob.setText(name)
+                ob.setAlignment(Qt.AlignRight)
+                self.settings_grid.addWidget(ob, x, y)
+                
+                
+    def define_io(self):
+        
+        self.io_counter = -1
+        def add_text_box(self, feature_type, text):
+            self.io_counter += 1
+            widget = feature_type(self.settings_frame)
+            widget.setText(text)
+            self.settings_grid.addWidget(widget, self.io_counter, 1)
+            return widget
+        
+        def add_widget(self, wid_type):
+            self.io_counter += 1
+            widget = wid_type(self.settings_frame)
+            self.settings_grid.addWidget(widget, self.io_counter, 1)
+            return widget
+            
+        
+        current_dir = getcwd()
+        self.tb_stud_pref = add_text_box(self, QLineEdit, join(current_dir, 'student_preferences.csv'))
+        self.tb_fac_pref = add_text_box(self, QLineEdit, join(current_dir, 'faculty_preferences.csv'))
+        self.tb_stud_avail = add_text_box(self, QLineEdit, join(current_dir, 'student_availability.csv'))
+        self.tb_fac_avail = add_text_box(self, QLineEdit, join(current_dir, 'faculty_availability.csv'))
+        self.tb_results_dir = add_text_box(self, QLineEdit, join(current_dir, 'results'))
+        self.tb_stud_sched_dir = add_text_box(self, QLineEdit, join(current_dir, 'results', 'student_schedules'))
+        self.tb_fac_sched_dir = add_text_box(self, QLineEdit, join(current_dir, 'results', 'faculty_schedules'))
+        self.sb_num_inter = add_widget(self, QSpinBox)
+        self.sb_min_num_inter = add_widget(self, QSpinBox)
+        self.sb_max_num_inter = add_widget(self, QSpinBox)
+        self.cb_extra_match = add_widget(self, QCheckBox)
+        self.sb_fac_advantage = add_widget(self, QSpinBox)
+        
+        # self tb_stud_pref =
+    
+    
+    def define_controls(self):
+        
+        def add_button(self, text):
+            button = QPushButton(self.button_frame)
+            button.setText(text)
+            self.button_layout.addWidget(button)
+            return button
+        
+        self.bt_save = add_button(self, 'Save Settings')
+        self.bt_load = add_button(self, 'Load Settings')
+        
+        
+        # self.bt_save = QPushButton(self.button_frame)
+        # self.bt_save
+        
+        
+        # self.buttons = {(0, 0): 'Save Settings',
+        #                 (0, 1): 'Load Settings'}
+        
+        # for pos, name in self.buttons.items():
+        #         x, y = pos
+        #         ob = QPushButton(self.button_frame)
+        #         ob.setText(name)
+        #         self.button_layout.addWidget(ob)
+    
         
     
     def define_text_boxes(self):
         current_dir = getcwd()
-        self.text_boxes = {(0, 1): join(current_dir + 'student_preferences.csv'),
-                           (1, 1): join(current_dir + 'faculty_preferences.csv'),
-                           (2, 1): join(current_dir + 'student_availability.csv'),
-                           (3, 1): join(current_dir + 'faculty_availability.csv'),
-                           (4, 1): join(current_dir + 'results'),
-                           (5, 1): join(current_dir + 'results', 'student_schedules'),
-                           (6, 1): join(current_dir + 'results', 'faculty_schedules'),
-                           (7, 1): "9",
-                           (8, 1): "3",
-                           (9, 1): "9",
-                           (10, 1): "True",
-                           (11, 1): "90"}
-    
-    def define_buttons(self):
-        self.buttons = {(12, 0): 'Save Settings',
-                        (12, 1): 'Load Settings'}
-            
-    def populate_frame(self):
-        widgets_to_populate = [self.labels, self.text_boxes, self.buttons]
-        widgets_types = [QLabel, QTextEdit, QPushButton]
         
-        for wid, wid_type in zip(widgets_to_populate, widgets_types):
-            for pos, name in wid.items():
-                x, y = pos
-                ob = wid_type(self)
-                ob.setText(name)
-                self.settings_grid.addWidget(ob, x, y)
+    
+    # def define_buttons(self):
+        
+            
+    # def populate_frame(self):
+    #     widgets_to_populate = [self.labels, self.text_boxes, self.buttons]
+    #     widgets_types = [QLabel, QTextEdit, QPushButton]
+        
+    #     for wid, wid_type in zip(widgets_to_populate, widgets_types):
+            
                 
                 
                 
