c917a9ac1b3e983968a5466a6cd5a0c8f0b71ced
Cale Crowder <dcc42@zips.uakron.edu>
2020-03-17 23:45:00 -0400

    gui.py - add checkboxes - will be deleting this file since it is tkinter and we are moving to pyqt5

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Checkbutton, IntVar, Tk, W, StringVar, Entry, Label
+from tkinter import Checkbutton, IntVar, Tk, W, StringVar, Entry, Label, Frame, E, Button
 
 
 
@@ -57,17 +57,37 @@ class defaults():
         self.COLUMN_WIDTH = 25
         self.EMPTY_PENALTY = 0
         
+# https://stackoverflow.com/questions/2395431/using-tkinter-in-python-to-edit-the-title-bar
+class tk_title(Frame):
+    
+    def __init__(self,parent=None, title='NoTitle'):
+        Frame.__init__(self,parent)
+        self.parent = parent
+        self.make_widgets(title)
+        
+    def make_widgets(self, title):
+        # don't assume that self.parent is a root window.
+        # instead, call `winfo_toplevel to get the root window
+        self.winfo_toplevel().title(title)
+
 
 class gui():
     
-    def __init__(self):
+    def __init__(self, matchmaker=None):
+        
+        # Matchmaker
+        if matchmaker != None:
+            self.mm = matchmaker
+        else:
+            self.mm = 'None'
         
         # Defaults
-        d = defaults()        
+        d = defaults()       
         self.d = d
         
         # Set up master
         self.master = Tk()
+        tk_title(self.master, 'MatchMakr') 
         
         # Define checkbox variables
         var_min_max = IntVar(value=d.USE_INTERVIEW_LIMITS)
@@ -76,42 +96,27 @@ class gui():
         var_stud_match_qualtiy = IntVar(value=d.PRINT_STUD_PREFERENCE)
         var_faculty_match_quality = IntVar(value=d.PRINT_FACULTY_PREFERENCE)
         var_ranked_pref = IntVar(value=d.USE_RANKING)
-        var_lunch_pref = IntVar(value=d.USE_WORK_LUNCH)
-        var_recruiting_pref = IntVar(value=d.USE_RECRUITING)
-        var_track_pref = IntVar(value=d.USE_TRACKS)
-        var_faculty_sim = IntVar(value=d.USE_FACULTY_SIMILARITY)
         var_stud_avail = IntVar(value=d.USE_STUDENT_AVAILABILITY)
         var_faculty_avail = IntVar(value=d.USE_INTERVIEW_LIMITS)
         
         # Checkbuttons
+        check_button_column = 3
         Checkbutton(self.master, text="Use min/max interview number",
-                    variable=var_min_max).grid(row=0, column=0, sticky=W)
+                    variable=var_min_max).grid(row=7, column=check_button_column, sticky=W)
         Checkbutton(self.master, text="Print suggestions for additional interviews",
-                    variable=var_suggestions).grid(row=1, column=0, sticky=W)
+                    variable=var_suggestions).grid(row=21, column=check_button_column, sticky=W)
         Checkbutton(self.master, text="Check if preferences are met (slower)",
-                    variable=var_check_preferences).grid(row=2, column=0, sticky=W)
-        
+                    variable=var_check_preferences).grid(row=22, column=check_button_column, sticky=W)   
         Checkbutton(self.master, text="Print match quality for students",
-                    variable=var_stud_match_qualtiy).grid(row=3, column=0, sticky=W)
+                    variable=var_stud_match_qualtiy).grid(row=23, column=check_button_column, sticky=W)
         Checkbutton(self.master, text="Print match quality for faculty",
-                    variable=var_faculty_match_quality).grid(row=4, column=0, sticky=W)
-        
-        
+                    variable=var_faculty_match_quality).grid(row=24, column=check_button_column, sticky=W)
         Checkbutton(self.master, text="Use ranked preferences",
-                    variable=var_ranked_pref).grid(row=5, column=0, sticky=W)
-        Checkbutton(self.master, text="Consider preferences for working over lunch",
-                    variable=var_lunch_pref).grid(row=6, column=0, sticky=W)
-        Checkbutton(self.master, text="Give recruiting faculty an advantage",
-                    variable=var_recruiting_pref).grid(row=7, column=0, sticky=W)
-        Checkbutton(self.master, text="Give matches with the same track an advantage",
-                    variable=var_track_pref).grid(row=8, column=0, sticky=W)
-        Checkbutton(self.master, text="Give similar faculty an advantage",
-                    variable=var_faculty_sim).grid(row=9, column=0, sticky=W)
-        
+                    variable=var_ranked_pref).grid(row=10, column=check_button_column, sticky=W)
         Checkbutton(self.master, text="Use student availability",
-                    variable=var_stud_avail).grid(row=10, column=0, sticky=W)
+                    variable=var_stud_avail).grid(row=5, column=check_button_column, sticky=W)
         Checkbutton(self.master, text="Use faculty availability",
-                    variable=var_faculty_avail).grid(row=11, column=0, sticky=W)
+                    variable=var_faculty_avail).grid(row=4, column=check_button_column, sticky=W)
         
         # String variables
         path = StringVar(value=d.PATH)
@@ -122,35 +127,37 @@ class gui():
         stud_avail = StringVar(value=d.STUDENT_AVAILABILITY_NAME)
         
         # String boxes
+        label_column = 0
+        entry_column = 1
         Label(self.master,
-                    text='File path:').grid(row=0, column=1, sticky=W)
+                    text='File path:').grid(row=0, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=path).grid(row=1, column=1, sticky=W)
+                    textvariable=path).grid(row=0, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Student preference file name:').grid(row=2, column=1, sticky=W)
+                    text='Student preference file name:').grid(row=1, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=stud_pref).grid(row=3, column=1, sticky=W)
+                    textvariable=stud_pref).grid(row=1, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Faculty preference file name:').grid(row=4, column=1, sticky=W)
+                    text='Faculty preference file name:').grid(row=2, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=faculty_pref).grid(row=5, column=1, sticky=W)
+                    textvariable=faculty_pref).grid(row=2, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Interview times file name:').grid(row=6, column=1, sticky=W)
+                    text='Interview times file name:').grid(row=3, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=interview_times).grid(row=7, column=1, sticky=W)
+                    textvariable=interview_times).grid(row=3, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Faculty availability file name:').grid(row=8, column=1, sticky=W)
+                    text='Faculty availability file name:').grid(row=4, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=faculty_avail).grid(row=9, column=1, sticky=W)
+                    textvariable=faculty_avail).grid(row=4, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Student availability file name:').grid(row=10, column=1, sticky=W)
+                    text='Student availability file name:').grid(row=5, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=stud_avail).grid(row=11, column=1, sticky=W)
+                    textvariable=stud_avail).grid(row=5, column=entry_column, sticky=W)
         
         # Entry variables
         num_interviews = IntVar(value=d.NUM_INTERVIEWS)
@@ -175,97 +182,126 @@ class gui():
         
         # Entry boxes
         Label(self.master,
-                    text='Number of interview slots (int > 0):').grid(row=0, column=2, sticky=W)
+                    text='Number of interview slots (int > 0):').grid(row=6, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=num_interviews).grid(row=1, column=2, sticky=W)
+                    textvariable=num_interviews).grid(row=6, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Minimum number of interviews (int > 0):').grid(row=2, column=2, sticky=W)
+                    text='Minimum number of interviews (int > 0):').grid(row=7, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=min_interviews).grid(row=3, column=2, sticky=W)
+                    textvariable=min_interviews).grid(row=7, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Maximum number of interviews (int >= 0):').grid(row=4, column=2, sticky=W)
+                    text='Maximum number of interviews (int >= 0):').grid(row=8, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=max_interviews).grid(row=5, column=2, sticky=W)
+                    textvariable=max_interviews).grid(row=8, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Faculty preference (ints [0, 100]):').grid(row=6, column=2, sticky=W)
+                    text='Faculty preference (ints [0, 100]):').grid(row=9, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=faculty_advantage).grid(row=7, column=2, sticky=W)
+                    textvariable=faculty_advantage).grid(row=9, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Maximum number of matches to consider (int > 0):').grid(row=8, column=2, sticky=W)
+                    text='Maximum number of matches to consider (int > 0):').grid(row=10, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=max_ranking).grid(row=9, column=2, sticky=W)
+                    textvariable=max_ranking).grid(row=10, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Preference given to first choice (int > 0):').grid(row=10, column=2, sticky=W)
+                    text='Preference given to first choice (int > 0):').grid(row=11, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=choice_exponent).grid(row=11, column=2, sticky=W)
+                    textvariable=choice_exponent).grid(row=11, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Lunch penalty (int > 0):').grid(row=12, column=2, sticky=W)
+                    text='Lunch penalty (int > 0):').grid(row=12, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=lunch_penalty).grid(row=13, column=2, sticky=W)
+                    textvariable=lunch_penalty).grid(row=12, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Interview period containing lunch (int > 0):').grid(row=14, column=2, sticky=W)
+                    text='Interview period containing lunch (int > 0):').grid(row=13, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=lunch_period).grid(row=15, column=2, sticky=W)
+                    textvariable=lunch_period).grid(row=13, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Recruiting faculty advantage (int > 0):').grid(row=16, column=2, sticky=W)
+                    text='Recruiting faculty advantage (int > 0):').grid(row=14, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=recruiting_weight).grid(row=17, column=2, sticky=W)
+                    textvariable=recruiting_weight).grid(row=14, column=entry_column, sticky=W)
          
         Label(self.master,
-                    text='Maximum solver time in seconds (int > 0):').grid(row=18, column=2, sticky=W)
+                    text='Maximum solver time in seconds (int > 0):').grid(row=15, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=max_solver_time_seconds).grid(row=19, column=2, sticky=W)
+                    textvariable=max_solver_time_seconds).grid(row=15, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Penalty for empty interview slots  (int > 0, AVOID USING):').grid(row=20, column=2, sticky=W)
+                    text='Penalty for empty interview slots \n(int > 0, AVOID USING):').grid(row=16, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=empty_penalty).grid(row=21, column=2, sticky=W)
-        
-        # ------------------------------------------------------------------------------------------
-        Label(self.master,
-                    text='Track advantage (int > 0):').grid(row=12, column=1, sticky=W)
-        Entry(self.master,
-                    textvariable=track_weight).grid(row=13, column=1, sticky=W)
+                    textvariable=empty_penalty).grid(row=16, column=entry_column, sticky=W)
         
         Label(self.master,
-                    text='Faculty similarity advantage (int > 0):').grid(row=14, column=1, sticky=W)
+                    text='Track advantage (int > 0):').grid(row=17, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=faculty_sim_weight).grid(row=15, column=1, sticky=W)
+                    textvariable=track_weight).grid(row=17, column=1, sticky=W)
         
         Label(self.master,
-                    text='Number of similar faculty to match (int > 0):').grid(row=16, column=1, sticky=W)
+                    text='Faculty similarity advantage (int > 0):').grid(row=18, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=num_similar_faculty).grid(row=17, column=1, sticky=W)
+                    textvariable=faculty_sim_weight).grid(row=18, column=1, sticky=W)
         
         Label(self.master,
-                    text='Number of preferences to check during matching (int > 0):').grid(row=18, column=1, sticky=W)
+                    text='Number of similar faculty to match (int > 0):').grid(row=19, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=num_pref_2_check).grid(row=19, column=1, sticky=W)
+                    textvariable=num_similar_faculty).grid(row=19, column=1, sticky=W)
         
         Label(self.master,
-                    text='Number of interviews to suggest (int > 0):').grid(row=20, column=1, sticky=W)
+                    text='Number of preferences to check \n during matching (int > 0):').grid(row=20, column=label_column, sticky=E)
+        Entry(self.master,
+                    textvariable=num_pref_2_check).grid(row=20, column=1, sticky=W)
+        
+        Label(self.master,
+                    text='Number of interviews to suggest (int > 0):').grid(row=21, column=label_column, sticky=E)
         Entry(self.master,
                     textvariable=num_suggestions).grid(row=21, column=1, sticky=W)
         
         Label(self.master,
-                    text='Number of iterations to run before checking progress  (int > 0, big = slow):').grid(row=22, column=1, sticky=W)
+                    text='Number of iterations to run before checking\nprogress  (int > 0, big = slow):').grid(row=22, column=label_column, sticky=E)
         Entry(self.master,
-                    textvariable=check_frequency).grid(row=23, column=1, sticky=W)
-       
+                    textvariable=check_frequency).grid(row=22, column=1, sticky=W)
+        
+        # Run Button
+        button_column = 4
+        Button(self.master, text='Run', command=self.start_matchmaking).grid(row=24, column = button_column)
+        
+        # Feedback box
+#        self.feedback =Label(self.master, text='test', width=20, height=10, bg='red').grid(row=0, column=4, sticky=W)
         
         # Mainloop
         self.master.mainloop()
         
+    def start_matchmaking(self):
+        fb = feedback()
+        self.mm.main(fb)        
         
         
+    
+class feedback():
+    
+    def __init__(self):
+        
+        # Constants
+        self.NUM_TEXT_LINES = 200
+        self.text_var = StringVar(value='test')
+        
+        # Make master form
+        self.master = Tk()
+        tk_title(self.master, 'MatchMakr - Feedback') 
+        
+        self.label = Label(self.master, text='Test', width=100, height=50)
+        self.label.pack()
+        
+        self.master.update()
+        
+    def update_text(self, text):
+        self.label.config(text=text)    
+        self.update()
 
 
 if __name__=='__main__':
