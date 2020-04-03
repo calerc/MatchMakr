654fb715cda20eae370b008a0bc79de0f8730c03
Cale Crowder <dcc42@zips.uakron.edu>
2020-04-03 01:05:07 -0400

    Renamed linux setup scripts, since they work in MacOS as well

--- setup/pyinstaller_linux.sh
+++
@@ -1,7 +0,0 @@
-
-# Use this script to freeze (compile) MatchMakr from this directory
-# Resulting executable is in ./dist
-# Ensure that the MatchMakr virtual environment is activated before freezing
-
-
-pyinstaller --onefile ../src/MatchMakr.py
